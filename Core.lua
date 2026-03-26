3f6df46a0b27973d9bf3e7c645610b7cc49e0a2f
GrumpyPlayer <95697160+GrumpyPlayer@users.noreply.github.com>
2026-03-26 11:36:53 -0400

    additional debugging of payloads

--- Core.lua
+++ Core.lua
@@ -26,7 +26,19 @@ function Core:SendCommMessage(prefix, text, distribution, target, prio, callback
         return
     end
 
-    GBankClassic_Output:Debug("COMMS", ">", prefix, prefixDesc, "via", string.upper(distribution), "to", target and GBankClassic_Chat:ColorPlayerName(target) or "guild", "(" .. (#text or 0) .. " bytes)")
+    --GBankClassic_Output:Debug("COMMS", ">", prefix, prefixDesc, "via", string.upper(distribution), "to", target and GBankClassic_Chat:ColorPlayerName(target) or "guild", "(" .. (#text or 0) .. " bytes)")
+	local success, data = GBankClassic_Core:DeSerializePayload(text)
+    local tablePayload = {}
+    local payload
+    if type(data) == "table" then
+      for k, v in pairs(data) do
+        table.insert(tablePayload, k .. "=" .. tostring(v))
+      end
+      payload = table.concat(tablePayload, ",")
+    else
+      payload = data
+    end
+    GBankClassic_Output:Debug("COMMS", ">", prefix, prefixDesc, "via", string.upper(distribution), "to", target and target or "guild", "(" .. (#text or 0) .. " bytes)", "payload:", payload)
 
     return AceComm_SendCommMessage(self, prefix, text, distribution, target, prio, callbackFn, callbackArg)
 end
--- Modules/Chat.lua
+++ Modules/Chat.lua
@@ -598,7 +598,18 @@ function Chat:OnCommReceived(prefix, message, distribution, sender)
         return
 	end
 
-	GBankClassic_Output:Debug("COMMS", "<", prefix, prefixDesc, "via", string.upper(distribution), "from", self:ColorPlayerName(sender), "(" .. (#message or 0) .. " bytes" .. (data.type and ", type=" .. tostring(data and data.type) or "") ..")")
+	-- GBankClassic_Output:Debug("COMMS", "<", prefix, prefixDesc, "via", string.upper(distribution), "from", self:ColorPlayerName(sender), "(" .. (#message or 0) .. " bytes" .. (data.type and ", type=" .. tostring(data and data.type) or "") ..")")
+    local tablePayload = {}
+    local payload
+    if type(data) == "table" then
+      for k, v in pairs(data) do
+        table.insert(tablePayload, k .. "=" .. tostring(v))
+      end
+      payload = table.concat(tablePayload, ",")
+    else
+      payload = data
+    end
+	GBankClassic_Output:Debug("COMMS", "<", prefix, prefixDesc, "via", string.upper(distribution), "from", sender, "(" .. (#message or 0) .. " bytes" .. (data.type and ", type=" .. tostring(data and data.type) or "") ..")", "payload:", payload)
 
 	if prefix == "gbank-dv2" then
 		if self:QueueDebouncedMessageWithMultipleGuildBankAlts(sender, data) then
@@ -733,6 +744,15 @@ function Chat:OnCommReceived(prefix, message, distribution, sender)
 				end
 			end
 		end
+
+		-- Print versions after a quiet period
+		if self.printVersionsTimer then
+			GBankClassic_Core:CancelTimer(self.printVersionsTimer)
+			self.printVersionsTimer = nil
+		end
+		self.printVersionsTimer = GBankClassic_Core:ScheduleTimer(function()
+			GBankClassic_Chat:PrintVersions()
+		end, 15)
 	end
 
 	if prefix == "gbank-s" then
